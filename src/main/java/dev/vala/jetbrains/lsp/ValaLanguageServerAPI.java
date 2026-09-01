bd705b1c5c339dcf34a774f97f807cd555477fe5
Trevor Busk <trevor@tbusk.com>
2026-08-31 20:29:17 -0400

    change: remove existing content

--- src/main/java/dev/vala/jetbrains/lsp/ValaLanguageServerAPI.java
+++
@@ -1,6 +0,0 @@
-package dev.vala.jetbrains.lsp;
-
-import org.eclipse.lsp4j.services.LanguageServer;
-
-public interface ValaLanguageServerAPI extends LanguageServer {
-}
--- src/main/java/dev/vala/jetbrains/project/ValaProjectSettings.java
+++
@@ -1,4 +0,0 @@
-package dev.vala.jetbrains.project;
-
-public class ValaProjectSettings {
-}
