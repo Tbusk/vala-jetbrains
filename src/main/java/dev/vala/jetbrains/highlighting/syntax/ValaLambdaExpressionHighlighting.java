ff53a0e25b49d72f9b9bd9fc6d7d60ba3a17b193
Trevor Busk <trevor@tbusk.com>
2026-09-05 08:27:20 -0400

    change: remove unused import

--- src/main/java/dev/vala/jetbrains/highlighting/syntax/ValaLambdaExpressionHighlighting.java
+++ src/main/java/dev/vala/jetbrains/highlighting/syntax/ValaLambdaExpressionHighlighting.java
@@ -6,7 +6,6 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.TokenSet;
 import dev.vala.jetbrains.highlighting.ValaHighlighter;
 import dev.vala.jetbrains.highlighting.ValaHighlighterUtil;
-import dev.vala.jetbrains.highlighting.ValaSyntaxHighlightingAnnotator;
 import dev.vala.jetbrains.highlighting.ValaTextAttributeKey;
 import dev.vala.jetbrains.parser.psi.ValaTypes;
 import dev.vala.jetbrains.parser.psi.impl.ValaLambdaExpressionImpl;
